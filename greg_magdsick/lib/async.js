0b9b261de8e1a0524907c3b92702556387243d30
Greg Magdsick <greg.magdsick@gmail.com>
2016-04-07 10:08:58 -0700

    get files to return in order

--- greg_magdsick/lib/async.js
+++ greg_magdsick/lib/async.js
@@ -1,1 +1,28 @@
 const fs = require('fs');
+const EE = require('events')
+
+var ee = new EE();
+
+fs.readFile('one.txt', (err, data) => {
+  if(err) console.log(err);
+  var buffer = Buffer(data)
+  console.log('first 8 bits in hex of one.txt: ' + buffer.toString('hex').slice(0,2));
+  ee.emit('oneDone');
+});
+
+ee.on('oneDone', () => {
+  fs.readFile('two.txt', (err, data) => {
+    if(err) console.log(err);
+    var buffer = Buffer(data)
+    console.log('first 8 bits in hex of two.txt: ' + buffer.toString('hex').slice(0,2));
+    ee.emit('twoDone');
+  });
+});
+
+ee.on('twoDone', () => {
+  fs.readFile('three.txt', (err, data) => {
+    if(err) console.log(err);
+    var buffer = Buffer(data)
+    console.log('first 8 bits in hex of three.txt: ' + buffer.toString('hex').slice(0,2));
+  });
+});
