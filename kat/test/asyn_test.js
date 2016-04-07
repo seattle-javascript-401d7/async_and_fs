2e813ff47a60da9b86d67c5235e6537b25c59a03
Katherine Beame <katherine.beame@gmail.com>
2016-04-07 11:50:16 -0700

    added an array test to check the order in which the functions are run

--- kat/test/asyn_test.js
+++ kat/test/asyn_test.js
@@ -4,10 +4,14 @@ const EventEmitter = require('events');
 
 const emitter = new EventEmitter();
 
+var order = [];
+var correctOrder = ['46696c65204f6e65', '46696c652074776f', '66696c6520332068'];
+
 describe('First File should', function () {
   it('console.log should read "46696c65204f6e65"', function (done) {
     fs.readFile(__dirname + '/../one.txt', function (err, data) {
       emitter.emit('one:done', data);
+      order.push(data.toString('hex', 0, 8));
       expect(data.toString('hex', 0, 8)).to.eql('46696c65204f6e65');
       expect(err).to.eql(null);
       done();
@@ -19,6 +23,7 @@ describe('Second file should', function () {
   it('console.log should read "46696c652074776f"', function (done) {
     fs.readFile(__dirname + '/../two.txt', function (err, data) {
       emitter.emit('two:done', data);
+      order.push(data.toString('hex', 0, 8));
       expect(data.toString('hex', 0, 8)).to.eql('46696c652074776f');
       expect(err).to.eql(null);
       done();
@@ -30,9 +35,18 @@ describe('Third file should', function () {
   it('console.log should read "66696c6520332068"', function (done) {
     fs.readFile(__dirname + '/../three.txt', function (err, data) {
       emitter.emit('three:done', data);
+      order.push(data.toString('hex', 0, 8));
       expect(data.toString('hex', 0, 8)).to.eql('66696c6520332068');
       expect(err).to.eql(null);
       done();
     });
+  });
+});
+
+describe('Correct order of results', function () {
+  it('the order of the results is "46696c65204f6e65", "46696c652074776f", "66696c6520332068"', function (done) {
+    expect(order).to.eql(correctOrder);
+    expect(true).to.eql(false);
+    done();
   });
 });
