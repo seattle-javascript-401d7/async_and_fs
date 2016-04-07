2b7f3c248b84df6cc104b38f5525540d998b72eb
Greg Magdsick <greg.magdsick@gmail.com>
2016-04-07 12:30:15 -0700

    moving towards functioning tests

--- greg_magdsick/lib/async.js
+++ greg_magdsick/lib/async.js
@@ -1,20 +1,24 @@
 const fs = require('fs');
 const EE = require('events')
 
-var ee = new EE();
-var results = [];
 
-ee.on('done', (fileArray) => {
-  var nextFile = fileArray.pop();
-  if (!nextFile) return console.log('done, results array: [' + results + ']');
-  fs.readFile(nextFile, (err, data) => {
-    if (err) return console.log(err);
+  var ee = new EE();
+  ee.results = [];
+  ee.files = ['three.txt', 'two.txt', 'one.txt'];
 
-    console.log('first 8 bits in hex of ' + nextFile + ': ' + data.toString('hex').slice(0,2));
-    results.push(data.toString('hex').slice(0,2))
-    ee.emit('done', fileArray);
+module.exports = exports = ee.on('done', (fileArray) => {
+    var nextFile = fileArray.pop();
+    if (!nextFile) {
+      ee.emit('finished');
+      return console.log('done, results array: [' + ee.results + ']');
+    }
+    fs.readFile(nextFile, (err, data) => {
+      if (err) return console.log(err);
+
+      console.log('first 8 bits in hex of ' + nextFile + ': ' + data.toString('hex').slice(0,2));
+      ee.results.push(data.toString('hex').slice(0,2))
+      ee.emit('done', fileArray);
+    });
   });
-});
 
-var files = ['three.txt', 'two.txt', 'one.txt'];
-ee.emit('done', files);
+  ee.emit('done', ee.files);
--- greg_magdsick/test/async_test.js
+++ greg_magdsick/test/async_test.js
@@ -1,1 +1,19 @@
-const 
+const async = require(__dirname + '/../lib/async');
+const expect = require('chai').expect;
+const fs = require('fs');
+const EE = require('events');
+
+const ee = new EE;
+
+ee.emit('done', ['three.txt', 'two.txt', 'one.txt']);
+
+describe('async test', () => {
+  it('one.txt fires first', (done) => {
+    fs.readFile('one.txt', (err, data) => {
+      ee.on('finished', (fileArray, results) => {
+        expect(data.toString('hex').slice(0,2)).to.eql(ee.results[0]);
+        done();
+      })
+    })
+  })
+})
