57e38cbdbc9c213586b67b0af7bd98d2d9d6dcaf
Philip A. Hirt <phirt13@gmail.com>
2016-04-07 12:14:53 -0700

    finished test and async

--- alex_hirt/lib/async-fs.js
+++ alex_hirt/lib/async-fs.js
@@ -1,16 +1,41 @@
-const fs = require('fs');
+'use strict';
 
-function read() {
-  fs.readFile('../one.txt', function(error, data) {
+const fs = require('fs');
+const EE = require('events');
+
+const ee = module.exports = new EE();
+
+ee.on('runOne', function() {
+  console.log('running one');
+  fs.readFile(__dirname + '/../one.txt', function(error, data) {
     if(error) {
       return console.log(error);
     }
     console.log(data.toString('hex', 0, 8));
+
+    ee.emit('runTwo');
   });
-}
+});
 
-read();
+ee.emit('runOne');
 
-process.nextTick(function() {
- console.log('inside next tick');
-})
+ee.on('runTwo', function() {
+  console.log('running two');
+   fs.readFile(__dirname + '/../two.txt', function(error, data) {
+    if(error) {
+      return console.log(error);
+    }
+    console.log(data.toString('hex', 0, 8));
+    ee.emit('runThree');
+  });
+});
+
+ee.on('runThree', function() {
+  console.log('running three');
+  fs.readFile(__dirname + '/../three.txt', function(error, data) {
+    if(error) {
+      return console.log(error)
+    }
+    console.log(data.toString('hex', 0, 8));
+  });
+});
