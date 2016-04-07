5d630552a2e2f596e53e4f5e8efb0e5c6b1d3b7f
Katherine Beame <katherine.beame@gmail.com>
2016-04-07 08:23:48 -0700

    first attempt at a succesful test

--- kat/test/asyn_test.js
+++ kat/test/asyn_test.js
@@ -1,11 +1,20 @@
 const expect = requier('chai').expect;
 
-describe('Should run an order', function () {
 
-
-
+describe('Should run in order', function () {
 
   it('console.log should read "File One"', function () {
-    expect(fs)
-  })
+  //call the function that you are testing
+  var result = fs.readFile('one.txt', function (err, data) {
+    if (err) return console.log(err);
+    emitter.emit('one:done', data);
+  });
+//assert
+result.then(function(data) {
+  expect(data).to.eql('one:done');
+  done();
+}, function(error) {
+  assert.fail(error);
+  done();
+});
 });
