0907b60e71441f6b974a0c92265ff09d8218a44b
Katherine Beame <katherine.beame@gmail.com>
2016-04-07 09:15:13 -0700

    attempt at creating a promise using Q library

--- kat/lib/fs.js
+++ kat/lib/fs.js
@@ -3,27 +3,27 @@ const EventEmitter = require('events');
 
 const emitter = new EventEmitter();
 
-fs.readFile('one.txt', function (err, data) {
+fs.readFile(__dirname + '/../one.txt', function (err, data) {
   if (err) return console.log(err);
   emitter.emit('one:done', data);
 });
 
 emitter.on('one:done', function (data) {
-  console.log(data.toString('utf-8', 0, 8));
-  fs.readFile('two.txt', function (err, data) {
+  console.log(data.toString('hex', 0, 8));
+  fs.readFile(__dirname + '/../two.txt', function (err, data) {
     if (err) return console.log(err);
-    emitter.emit('one:two', data);
+    emitter.emit('two:done', data);
   });
 });
 
 emitter.on('two:done', function (data) {
-  console.log(data.toString('utf-8', 0, 8));
-  fs.readFile('three.txt', function (err, data) {
+  console.log(data.toString('hex', 0, 8));
+  fs.readFile(__dirname + '/../three.txt', function (err, data) {
     if (err) return console.log(err);
     emitter.emit('three:done', data);
   });
 });
 
 emitter.on('three:done', function (data) {
-  console.log(data.toString('utf-8', 0, 8));
+  console.log(data.toString('hex', 0, 8));
 });
--- kat/test/asyn_test.js
+++ kat/test/asyn_test.js
@@ -1,20 +1,24 @@
-const expect = requier('chai').expect;
+const expect = require('chai').expect;
+const fs = require('fs');
+const q = require('q');
+const EventEmitter = require('events');
 
+const emitter = new EventEmitter();
 
-describe('Should run in order', function () {
-
-  it('console.log should read "File One"', function () {
+describe('Should print', function () {
+  it('console.log should read "46696c65204f6e65"', function (done) {
   //call the function that you are testing
-  var result = fs.readFile('one.txt', function (err, data) {
-    if (err) return console.log(err);
-    emitter.emit('one:done', data);
+    var result = fs.readFile(__dirname + '/../one.txt', function (err, data) {
+      if (err) return console.log(err);
+      emitter.emit('one:done', data);
+    });
+    //create a promise
+    q.nfcall(result).then(function(data) {
+      expect(data).to.eql('46696c65204f6e65');
+      done();
+    }, function(error) {
+      assert.fail(error);
+      done();
+    });
   });
-//assert
-result.then(function(data) {
-  expect(data).to.eql('one:done');
-  done();
-}, function(error) {
-  assert.fail(error);
-  done();
-});
 });
