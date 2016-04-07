2980547d4ae061cb08fb236ae1cd8ea411e7f19e
Katherine Beame <katherine.beame@gmail.com>
2016-04-07 09:45:06 -0700

    created a single working test for asynch using mocha done

--- kat/test/asyn_test.js
+++ kat/test/asyn_test.js
@@ -1,24 +1,19 @@
 const expect = require('chai').expect;
 const fs = require('fs');
-const q = require('q');
 const EventEmitter = require('events');
-
+const assert = require('assert');
 const emitter = new EventEmitter();
 
 describe('Should print', function () {
   it('console.log should read "46696c65204f6e65"', function (done) {
-  //call the function that you are testing
-    var result = fs.readFile(__dirname + '/../one.txt', function (err, data) {
-      if (err) return console.log(err);
-      emitter.emit('one:done', data);
-    });
-    //create a promise
-    q.nfcall(result).then(function(data) {
-      expect(data).to.eql('46696c65204f6e65');
-      done();
-    }, function(error) {
-      assert.fail(error);
-      done();
+  fs.readFile(__dirname + '/../one.txt', function (err, data) {
+    if (err) return console.log(err);
+    emitter.emit('one:done', data);
+    expect(data.toString('hex', 0, 8)).to.eql('46696c65204f6e65');
+    done();
+  });
+  }, function(error) {
+    assert.fail(error);
+    done();
     });
   });
-});
