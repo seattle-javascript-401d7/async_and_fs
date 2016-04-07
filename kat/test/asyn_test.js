d6700d9e2dec48ae9024283c077fc4bfff875570
Katherine Beame <katherine.beame@gmail.com>
2016-04-07 10:45:27 -0700

    added package.json and gulpfile.js to lint programs

--- kat/test/asyn_test.js
+++ kat/test/asyn_test.js
@@ -1,48 +1,39 @@
 const expect = require('chai').expect;
 const fs = require('fs');
 const EventEmitter = require('events');
-const assert = require('assert');
 
 const emitter = new EventEmitter();
 
 describe('First File should', function () {
   it('console.log should read "46696c65204f6e65"', function (done) {
     fs.readFile(__dirname + '/../one.txt', function (err, data) {
-      if (err) return console.log(err);
       emitter.emit('one:done', data);
       expect(data.toString('hex', 0, 8)).to.eql('46696c65204f6e65');
+      expect(err).to.eql(null);
       done();
     });
-  }, function(error) {
-    assert.fail(error);
-    done();
     });
   });
 
 describe('Second file should', function () {
   it('console.log should read "46696c652074776f"', function (done) {
     fs.readFile(__dirname + '/../two.txt', function (err, data) {
-      if (err) return console.log(err);
       emitter.emit('two:done', data);
       expect(data.toString('hex', 0, 8)).to.eql('46696c652074776f');
+      expect(err).to.eql(null);
       done();
     });
-  }, function(error) {
-    assert.fail(error);
-    done();
   });
 });
 
 describe('Third file should', function () {
   it('console.log should read "66696c6520332068"', function (done) {
     fs.readFile(__dirname + '/../three.txt', function (err, data) {
-      if (err) return console.log(err);
       emitter.emit('three:done', data);
       expect(data.toString('hex', 0, 8)).to.eql('66696c6520332068');
+      expect(err).to.eql(null);
       done();
     });
-  }, function(error) {
-    assert.fail(error);
-    done();
+
   });
 });
