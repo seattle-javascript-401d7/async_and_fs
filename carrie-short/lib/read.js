d91eee636c74454cd086c4742df478f96a3f112f
CarrieShort <carrie.r.phillips@gmail.com>
2016-04-07 09:21:06 -0700

    add test to check if files is an array

--- carrie-short/lib/read.js
+++ carrie-short/lib/read.js
@@ -2,5 +2,4 @@ const fs = require('fs');
 const EE = require('events');
 
 const ee = module.exports = new EE();
-
 ee.files = ['three.txt','two.txt','one.txt'];
--- carrie-short/test/read_test.js
+++ carrie-short/test/read_test.js
@@ -1,3 +1,9 @@
 const chai = require('chai');
 const expect = chai.expect;
 var ee = require(__dirname + '/../lib/read');
+
+describe('Files to be read', ()=>{
+  it('should be an array',()=>{
+    expect(Array.isArray(ee.files)).to.eql(true);
+  });
+});
