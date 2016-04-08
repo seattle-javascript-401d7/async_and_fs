1715a599f4b98eeac29a7e0beb4dfa1e0991720c
Greg Magdsick <greg.magdsick@gmail.com>
2016-04-08 12:15:09 -0700

    finished gulpFile testing

--- greg_magdsick/test/async_test.js
+++ greg_magdsick/test/async_test.js
@@ -3,7 +3,6 @@ const expect = require('chai').expect;
 
 
 describe('async test', () => {
-  debugger;
   beforeEach(() => {
     this.fileArray = [__dirname + '/../three.txt',
                       __dirname + '/../two.txt',
