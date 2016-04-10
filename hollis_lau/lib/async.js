bdba843470e28552a6bdc4850ed3e47f70622cf2
Hollis Lau <hollislau@hotmail.com>
2016-04-09 22:33:50 -0700

    Refactor code to make it testable; add tests

--- hollis_lau/lib/async.js
+++ hollis_lau/lib/async.js
@@ -1,39 +1,39 @@
 const fs = require("fs");
 
-module.exports = (function () {
-  return {
-    counter: 0,
-    readArray: [null, null, null],
-    printArray: [],
+const FileParser = module.exports = function (files, cb, writeStream) {
+  this.files = files;
+  this.cb = cb;
+  this.writeStream = writeStream || process.stdout;
+  this.parsed = 0;
+  this.readArray = [];
+  this.printString = [];
+};
 
-    readFile: function (index, file) {
-      fs.readFile(file, function (err, data) {
-        var hex;
+FileParser.prototype.start = function () {
+  var file;
 
+  for (var i = 2; i >= 0; i--) {
+    file = this.files.pop();
+
+    ((i, file) => {
+      fs.readFile(file, (err, data) => {
         if (err) {
-          return console.log(err);
+          return process.stderr.write(err);
         }
 
-        hex = data.toString("hex", 0, 8);
-        this.readArray[index] = hex;
-        this.counter++;
+        this.readArray[i] = data.toString("hex", 0, 8);
+        this.parsed++;
 
-        if (this.counter === 3) {
-          this.printArray = this.readArray.reverse().join(", ");
-          console.log(this.printArray);
+        if (this.parsed === 3) {
+          this.printString = this.readArray.reverse().join(", ");
+          this.writeStream.write(this.printString + "\n");
+          return this.cb();
         }
-      }.bind(this));
-    },
+      });
+    })(i, file);
+  }
+};
 
-    readMyFiles: function () {
-      var file;
-      var filesArray = ["three.txt", "two.txt", "one.txt"];
-
-      for (var i = 2; i >= 0; i--) {
-        file = filesArray.pop();
-
-        this.readFile(i, file);
-      }
-    }
-  };
-})();
+var files = ["three.txt", "two.txt", "one.txt"];
+var fp = new FileParser(files, function () {});
+fp.start();
--- hollis_lau/test/async_test.js
+++ hollis_lau/test/async_test.js
@@ -1,11 +1,21 @@
-const mocha = require("mocha");
-const chai = require("chai").expect;
-const async = require(__dirname + "/lib/async");
+const expect = require("chai").expect;
+const FileParser = require(__dirname + "/../lib/async");
 
-describe("async", function () {
-  it("should return 8 bytes of hex", function (done) {
-    //none of this shit works
-    var printArray;
-    done();
+describe("async order", () => {
+  beforeEach(() => {
+    this.files = [__dirname + "/three.txt",
+                  __dirname + "/two.txt",
+                  __dirname + "/one.txt"];
+    this.testStream = {
+      write: function () {}
+    };
+  });
+
+  it("should return hex strings in order", (done) => {
+    var fp = new FileParser(this.files, function () {
+      expect(this.printString).to.eql("6f6e650a, 74776f2074776f20, 74687265650a");
+      done();
+    }, this.testStream);
+    fp.start();
   });
 });
