ac0b81e6310a56f66f847cafa48f605ff102454a
bonecrushereb <bonecrushereb@gmail.com>
2016-04-12 12:46:57 -0700

    fixed issues for resubmit

--- BenNolan/test/filereadingtest.js
+++ BenNolan/test/filereadingtest.js
@@ -1,4 +1,5 @@
-const expect = require('chai').expect;
+const chai = require('chai');
+const expect = chai.expect;
 const ee = require(__dirname + '/../lib/filereading');
 const fs = require('fs');
 
@@ -7,17 +8,18 @@ describe('files are in an array', () => {
     expect(Array.isArray(ee.files)).to.eql(true);
   });
 });
+
 describe('Check event emitter', () => {
-  debugger;
   it('should read files in order', (done) => {
     ee.on('done', (fileArray) => {
-      var nextFile = fileArray.pop();
-      if (!nextFile) done();
+    var nextFile = fileArray.pop();
+    if(!nextFile) done();
       expect(fileArray).to.not.include(nextFile);
       fs.readFile(nextFile, (err, data) => {
         if(err) return console.log(err);
         var hexData = data.toString('hex', 0, 8);
         expect(hexData).to.have.length(16);
+        console.log(hexData);
         ee.emit('done', fileArray);
       });
     });
