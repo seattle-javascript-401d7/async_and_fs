bfb0419c170caaa29aa1bd5dc17a97f4a89b3cb0
Katherine Beame <katherine.beame@gmail.com>
2016-04-20 11:43:28 -0700

    Got rid of test of test in asyn_test.js

--- kat/test/asyn_test.js
+++ kat/test/asyn_test.js
@@ -7,46 +7,43 @@ const emitter = new EventEmitter();
 describe('Correct order of results', function () {
   it('the order of the results is "46696c65204f6e65", "46696c652074776f", "66696c6520332068"', function (done) {
     expect(order).to.eql(correctOrder);
-var order = [];
-var correctOrder = ['46696c65204f6e65', '46696c652074776f', '66696c6520332068'];
+    var order = [];
+    var correctOrder = ['46696c65204f6e65', '46696c652074776f', '66696c6520332068'];
 
-describe('First File should', function () {
-  it('console.log should read "46696c65204f6e65"', function (done) {
-    fs.readFile(__dirname + '/../one.txt', function (err, data) {
-      emitter.emit('one:done', data);
-      order.push(data.toString('hex', 0, 8));
-      expect(data.toString('hex', 0, 8)).to.eql('46696c65204f6e65');
-      expect(err).to.eql(null);
-      done();
+    describe('First File should', function () {
+      it('console.log should read "46696c65204f6e65"', function (done) {
+        fs.readFile(__dirname + '/../one.txt', function (err, data) {
+          emitter.emit('one:done', data);
+          order.push(data.toString('hex', 0, 8));
+          expect(data.toString('hex', 0, 8)).to.eql('46696c65204f6e65');
+          expect(err).to.eql(null);
+          done();
+        });
+      });
     });
-  });
-});
 
-describe('Second file should', function () {
-  it('console.log should read "46696c652074776f"', function (done) {
-    fs.readFile(__dirname + '/../two.txt', function (err, data) {
-      emitter.emit('two:done', data);
-      order.push(data.toString('hex', 0, 8));
-      expect(data.toString('hex', 0, 8)).to.eql('46696c652074776f');
-      expect(err).to.eql(null);
-      done();
+    describe('Second file should', function () {
+      it('console.log should read "46696c652074776f"', function (done) {
+        fs.readFile(__dirname + '/../two.txt', function (err, data) {
+          emitter.emit('two:done', data);
+          order.push(data.toString('hex', 0, 8));
+          expect(data.toString('hex', 0, 8)).to.eql('46696c652074776f');
+          expect(err).to.eql(null);
+          done();
+        });
+      });
     });
-  });
-});
 
-describe('Third file should', function () {
-  it('console.log should read "66696c6520332068"', function (done) {
-    fs.readFile(__dirname + '/../three.txt', function (err, data) {
-      emitter.emit('three:done', data);
-      order.push(data.toString('hex', 0, 8));
-      expect(data.toString('hex', 0, 8)).to.eql('66696c6520332068');
-      expect(err).to.eql(null);
-      done();
+    describe('Third file should', function () {
+      it('console.log should read "66696c6520332068"', function (done) {
+        fs.readFile(__dirname + '/../three.txt', function (err, data) {
+          emitter.emit('three:done', data);
+          order.push(data.toString('hex', 0, 8));
+          expect(data.toString('hex', 0, 8)).to.eql('66696c6520332068');
+          expect(err).to.eql(null);
+          done();
+        });
+      });
     });
-  });
-});
-
-    // expect(true).to.eql(false);
-    done();
   });
 });
