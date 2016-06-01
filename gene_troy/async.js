bc2b9d7ee2893cc8aa071c134760b3c439a891d3
Gene Troy <genetroy@hotmail.com>
2016-06-01 12:36:46 -0700

    refactor, promises finally work

--- gene_troy/async.js
+++ gene_troy/async.js
@@ -1,33 +1,17 @@
 const fs = require('fs');
+var files = ['one.txt', 'two.txt', 'three.txt'];
 
-// Promise creator that reads a file and processes the data
-var readYoFile = (filename, callback) => {
-  var cb = callback || function() {}; // if no callback available, give an empty function
-  return new Promise((resolve, reject) => { // promise constructor function
-    fs.readFile(filename, (err, data) => {
-      data = data.toString('hex',0,8); //convert to hex and slice off first 8 bytes
-      if (err) {
-        reject(err);
-        return cb(err);
-      }
-      resolve(data);
-      return cb(null, data);
+var promiseArray = module.exports = exports = files.map((file) => {
+  return new Promise((fulfill, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject(err);
+      fulfill([data, file]);
     });
   });
-};
-//Let's process some files
-readYoFile('one.txt')
-  .then((data) => {
-    console.log(data);
-    readYoFile('two.txt')
-      .then((data) => {
-        console.log(data);
-        readYoFile('three.txt')
-          .then((data) => {
-            console.log(data);
-          });
-      });
-  })
-  .catch((err) => {
-    throw err;
+});
+
+Promise.all(promiseArray).then((allPromises) => {
+  allPromises.forEach((ele) => {
+    console.log(ele[0].toString('hex', 0, 8) + ' ' + ele[1]);
   });
+});
